163d64afa260caabfc18d98222a90f6a32909626
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2025-01-10 17:28:33 +0100

    Implement user signup functionality with data storage and retrieval

---
+++ auth.py
@@ -0,0 +1,51 @@
+import psycopg2
+
+db_params = {
+    "dbname": "postgres",
+    "user": "postgres",
+    "password": "Oluwaseun123",
+    "host": "localhost",  
+}
+
+def insert_data(full_name, email, password):
+    insert_query = """
+        INSERT INTO ai_auth (full_name,email, password)
+        VALUES (%s, %s, %s)
+    """
+    data = (full_name,email, password)
+    try:
+        with psycopg2.connect(**db_params) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(insert_query, data)
+                conn.commit()
+                print("Data added successfully.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def get_all_data():
+    get_query = "SELECT email FROM ai_auth;"
+    try:
+        with psycopg2.connect(**db_params) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(get_query)
+                results = cursor.fetchall()
+                email_list = [row[0] for row in results]
+                print("Data retrieved successfully.")
+                return email_list
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+def get_signin_info():
+    get_query = "SELECT email, password FROM ai_auth;"
+    try:
+        with psycopg2.connect(**db_params) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(get_query)
+                results = cursor.fetchall()
+                print("Signin info retrieved successfully.")
+                return results
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from auth import insert_data,get_all_data
+import json
+
+origins = ['http://localhost:3000']
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+class Signup(BaseModel):
+    full_name:str
+    email:str
+    password:str
+
+
+
+@app.post("/api/signup")
+async def create_signup(input_data:Signup):
+    print(input_data)
+    full_name = input_data.full_name
+    email = input_data.email
+    password = input_data.password
+    with open("output.json","w") as f:
+        json.dump([{"email":email,"password":password}],f)
+    all_data = get_all_data()
+    # print(email)
+    # print(all_data)
+    if email not in all_data:
+        try:
+            insert_data(full_name,email,password)
+            print("Data added successfully")
+        except Exception as e:
+            print(f"Error occured adding data {e}")
+        return {"result":"no"}
+    else:
+        return {"result":"yes"}
+
+# @app.post("/api/signin")
+# async def create_signin(input_data:Signup):
+#     email = input_data.email
+#     password = input_data.password
